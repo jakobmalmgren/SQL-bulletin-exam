52393321e60c95cb0d497f2c4203a602f7c1db2c
Alexander <aleck_johansson@hotmail.com>
2025-05-16 13:24:59 +0200

    user Post

--- models/userModel.js
+++ models/userModel.js
@@ -1,1 +1,9 @@
-// HEJ
+import pool from '../db.js'
+
+export async function createUser(name, email) {
+    const result = await pool.query(
+        `INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *`,
+        [name, email]
+    );
+    return result.rows[0];
+}
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 import express from "express";
-import pool from "./db.js";
+/* import pool from "./db.js"; */
+import userRoutes from "./routes/userRoutes.js"
 
 const app = express();
 app.use(express.json());
 
-app.use("/api/users");
-app.use("/api/channels");
+app.use("/api/users", userRoutes)
+/* app.use("/api/channels");
 app.use("/api/subscriptions");
-app.use("/api/messages");
+app.use("/api/messages"); */
 
 app.listen(5000, () => {
   console.log(`servern körs på http://localhost:5000`);
